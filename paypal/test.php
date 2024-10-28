02376a79144acfec7bd6caf8e56a89cf2c9356d9
Rouie Ilustrisimo <ilustrisimo.rouie@outlook.com>
2024-10-28 23:06:16 +0800

    paypal

--- paypal/test.php
+++ paypal/test.php
@@ -6,4 +6,8 @@ use PayPalCheckoutSdk\Core\SandboxEnvironment;
 use PayPalCheckoutSdk\Core\ProductionEnvironment;
 use Dotenv\Dotenv;
 
+// Load environment variables from .env file
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 echo getenv('PAYPAL_CLIENT_ID');  // should print your PayPal Client ID
