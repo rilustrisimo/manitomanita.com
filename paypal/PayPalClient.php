aa21efdca92a2c254179c437819274e843aa2b8d
Rouie Ilustrisimo <ilustrisimo.rouie@outlook.com>
2024-10-28 23:11:10 +0800

    paypal

--- paypal/PayPalClient.php
+++ paypal/PayPalClient.php
@@ -16,11 +16,15 @@ class PayPalClient
         $dotenv->load();
 
         // Access the variables
-        $clientId = getenv('PAYPAL_CLIENT_ID');
-        $clientSecret = getenv('PAYPAL_CLIENT_SECRET');
-        $environment = getenv('PAYPAL_MODE') === 'sandbox' ?
-            new SandboxEnvironment($clientId, $clientSecret) :
-            new ProductionEnvironment($clientId, $clientSecret);
+        //$clientId = getenv('PAYPAL_CLIENT_ID');
+        //$clientSecret = getenv('PAYPAL_CLIENT_SECRET');
+        //$environment = getenv('PAYPAL_MODE') === 'sandbox' ?
+        //    new SandboxEnvironment($clientId, $clientSecret) :
+        //    new ProductionEnvironment($clientId, $clientSecret);
+
+        $clientId = 'Aev7owXcA7gNoB_lLSy9u2iikYz6Kt4YLFvtVnC-hjX7Z2DqjVpLB7Nxx-7l1ueYyDPeNQKt9_YtVcR0';
+        $clientSecret = 'ED1_WgCspRlKpGBna_0el69WI-Jv7Wsu-zvV6z3z9Zv0whVGHnFpfL10MBfMLNEsiBliNQi91zOhfGtH';
+        $environment = new SandboxEnvironment($clientId, $clientSecret);
         
         return new PayPalHttpClient($environment);
     }
