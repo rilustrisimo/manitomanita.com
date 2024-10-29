fcc3e787042b9939bf9da23b7c7379b3423be160
Rouie Ilustrisimo <ilustrisimo.rouie@outlook.com>
2024-10-29 13:06:58 +0800

    pro version

--- paypal/index-paypal.php
+++ paypal/index-paypal.php
@@ -1,9 +1,12 @@
 <div id="paypal-button-container"></div>
 <script src="https://www.paypal.com/sdk/js?client-id=Aev7owXcA7gNoB_lLSy9u2iikYz6Kt4YLFvtVnC-hjX7Z2DqjVpLB7Nxx-7l1ueYyDPeNQKt9_YtVcR0&currency=PHP"></script>
 <script>
+    const themeBaseUrl = '<?php echo get_template_directory_uri(); ?>';
+</script>
+<script>
     paypal.Buttons({
         createOrder: function(data, actions) {
-            return fetch('createOrder.php', {
+            return fetch(`${themeBaseUrl}/paypal/createOrder.php`, {
                 method: 'post'
             }).then(function(response) {
                 return response.json();
@@ -12,7 +15,7 @@
             });
         },
         onApprove: function(data, actions) {
-            return fetch('captureOrder.php', {
+            return fetch(`${themeBaseUrl}/paypal/captureOrder.php`, {
                 method: 'post',
                 headers: {
                     'content-type': 'application/json'
