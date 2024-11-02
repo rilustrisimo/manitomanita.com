5528fd37d3d7254e726ab56999cf4b482628b2cf
Rouie Ilustrisimo <ilustrisimo.rouie@outlook.com>
2024-11-02 11:55:08 +0800

    pro version

--- templates/pro/pro-btnpop.php
+++ templates/pro/pro-btnpop.php
@@ -18,9 +18,21 @@ defined( 'ABSPATH' ) || die();
 <div class="pop-container" id="make-pro">
     <div class="pop-container__inner">
         <div class="pop-container__close"><a href="#"><i class="fa-solid fa-xmark"></i></a></div>
-        <div class="pop-container__header">Make Your Group a <span>PRO</span></div>
+        <div class="pop-container__header">Manito Manita <span>PRO</span></div>
         <div class="pop-container__content">
-            <a href="#" id="make-pro-btn">Make it PRO</a>
+            <p>Unlock Advanced Moderator Tools</p>
+            <h1>₱199.00 PHP</h1>
+            <p>UOne-time payment</p>
+            <p class="heading">With Manito Manita PRO, Moderators Can:</p>
+            <ul>
+            <li><b>Reshuffle</b> group pairings</li>
+            <li><b>View</b> member names and screen names</li>
+            <li><b>Access</b> and review member matches/pairs</li>
+            <li><b>Remove</b> members from the group</li>
+            <li><b>Edit</b> member information</li>
+            <li><b>Export</b> group data</li>
+            </ul>
+            <a href="#" id="make-pro-btn">Upgrade to PRO</a>
             <div class="paypal-pay" style="display:none;">
                 <?php get_template_part( 'paypal/index', 'paypal' ); ?>
             </div>
