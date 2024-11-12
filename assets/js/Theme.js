5dcd9234d3c914f336fcfd937ed353ae2dc30d45
Rouie Ilustrisimo <ilustrisimo.rouie@outlook.com>
2024-11-12 14:46:19 +0800

    pro version

--- assets/js/Theme.js
+++ assets/js/Theme.js
@@ -69,11 +69,11 @@ var Theme = {
             // Check if the cookie exists
             if (getCookie('affiliateClick')) {
                 // Only open the href in the same tab if the cookie exists
-                window.location.href = href;
+                window.open(href, '_blank');
             } else {
                 // Open data-url in a new tab and href in the same tab, and set the cookie if it doesn't exist
-                window.open(dataUrl, '_blank');
-                window.location.href = href;
+                window.open(href, '_blank');
+                window.location.href = dataUrl;
     
                 // Set cookie to expire in 1 day
                 setCookie('affiliateClick', 'true', 1);
